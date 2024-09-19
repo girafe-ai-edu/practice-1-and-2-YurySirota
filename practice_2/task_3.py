127f30a1c3a789d46244ecae9551a7733c42151c
YurySirota <32647071+YurySirota@users.noreply.github.com>
2024-09-19 11:34:04 +0300

    Update task_3.py
    Юрий Сирота. Практика 2. задачи 3

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,12 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
-
+weight = float(input())
+height = float(input())
 
 #code here
+
+print( f" BMI =  { weight/(height**2) } " )
+
+
 
